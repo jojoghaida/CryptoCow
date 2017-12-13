64b6c96e1766e9d96986be9281adc5459eccb2f1
jojoghaida <josephghaida@gmail.com>
2017-12-13 16:59:05 -0500

    dsfsdf

--- CryptoCow.js
+++ CryptoCow.js
@@ -4,9 +4,15 @@ var date = new Date();
 console.log(date);
 
 var plotCanvas = document.getElementById('plotter');
+//offset height of toolbar and tabs menu!
+
 var sW = plotCanvas.offsetWidth
 var sH = plotCanvas.offsetHeight
 var params = {width: sW, height: sH};
 var two = new Two(params).appendTo(plotCanvas);
+
+var line = two.makeLine(sH/2,0,sW,sH);
+line.linewidth = 3;
+line.stroke = "black";
 
 //

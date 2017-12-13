ad93b6ca37df7592b6f108c79e1632339bef1045
jojoghaida <josephghaida@gmail.com>
2017-12-13 16:44:35 -0500

    added two scene

--- CryptoCow.js
+++ CryptoCow.js
@@ -1,5 +1,12 @@
 //Crypto Cow
-alert('welcome!');
+
 var date = new Date();
 console.log(date);
+
+var plotCanvas = document.getElementById('plotter');
+var sW = plotCanvas.offsetWidth
+var sH = plotCanvas.offsetHeight
+var params = {width: sW, height: sH};
+var two = new Two(params).appendTo(plotCanvas);
+
 //

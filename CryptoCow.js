9d085ae6ef3fc234b4dfd7a2189821b7e41927d3
jojoghaida <josephghaida@gmail.com>
2017-12-14 11:36:16 -0500

    gedetrd

--- CryptoCow.js
+++ CryptoCow.js
@@ -3,16 +3,21 @@
 var date = new Date();
 console.log(date);
 
+
 var plotCanvas = document.getElementById('plotter');
 //offset height of toolbar and tabs menu!
 
-var sW = plotCanvas.offsetWidth
-var sH = plotCanvas.offsetHeight
+function setCanvas(){
+  var sW = plotCanvas.offsetWidth
+  var sH = plotCanvas.offsetHeight
+}
+setCanvas();
+
 var params = {width: sW, height: sH};
 var two = new Two(params).appendTo(plotCanvas);
 
 var line = two.makeLine(0,sH-30,sW,sH-30);
-line.linewidth = 3;
+line.linewidth = 2;
 line.stroke = "black";
 
 var dateText = two.makeText(date,sW/2,sH-30+15);
@@ -22,5 +27,11 @@ dateText.family = 'Karla';
 dateText.value = date;
 
 two.update();
+
+
+function onWindowRotate() {
+  setCanvas();
+  two.update();
+}
 
 //

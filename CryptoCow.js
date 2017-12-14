a89d9d8eb4dcd035ae65afb8ab11115bde712fc6
jojoghaida <josephghaida@gmail.com>
2017-12-14 11:45:07 -0500

    fdvfdv

--- CryptoCow.js
+++ CryptoCow.js
@@ -15,11 +15,11 @@ var two = new Two(params).appendTo(plotCanvas);
 
 var graphOffsetHeight = 50;
 
-var line = two.makeLine(0,sH-30+graphOffsetHeight,sW,sH-30+graphOffsetHeight);
+var line = two.makeLine(0,sH-30-graphOffsetHeight,sW,sH-30-graphOffsetHeight);
 line.linewidth = 2;
 line.stroke = "black";
 
-var dateText = two.makeText(date,sW/2,sH-30+15+graphOffsetHeight);
+var dateText = two.makeText(date,sW/2,sH-30+15-graphOffsetHeight);
 dateText.fill = "black";
 dateText.size = 12;
 dateText.family = 'Karla';

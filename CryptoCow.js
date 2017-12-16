09d9a0e7b1aabfeee755d23050f4a1b48b93bb12
jojoghaida <josephghaida@gmail.com>
2017-12-16 00:49:32 -0500

    erferf

--- CryptoCow.js
+++ CryptoCow.js
@@ -22,7 +22,7 @@ var plotLine = two.makeLine(0,sH-graphOffsetHeight,sW,sH-graphOffsetHeight);
 plotLine.linewidth = 2;
 plotLine.stroke = "black";
 
-var zoom = two.makeText("zoom",sW/2,sH + 70 - graphOffsetHeight);
+var zoom = two.makeText("zoom",sW/2,sH + 75 - graphOffsetHeight);
 zoom.fill = "black";
 zoom.size = 30;
 zoom.family = 'Karla';

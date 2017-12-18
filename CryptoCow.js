2232cb77bfb6507bb88e834bec2b93f4a3d93594
jojoghaida <josephghaida@gmail.com>
2017-12-18 06:03:29 -0500

    gerge

--- CryptoCow.js
+++ CryptoCow.js
@@ -63,7 +63,7 @@ ltcPts.push(a,b,c,d);
 
 var ltcPath = two.makePath(ltcPts);
 
-var clrStop1 = new Two.Stop(.9,'white',0);
+var clrStop1 = new Two.Stop(.5,'white',0);
 var clrStop2 = new Two.Stop(1,'grey',1);
 var ltcGradient = two.makeLinearGradient(sW,sH-graphOffsetHeight,sW,0,clrStop1,clrStop2);
 ltcPath.linewidth = 2;
